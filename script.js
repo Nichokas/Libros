b93158d928b82efe1024eef525ac5955ca325006
Nichokas <67232385+Nichokas@users.noreply.github.com>
2023-04-16 13:03:03 +0000

    haaa

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-
+const books = document.querySelectorAll('.book');
+books.forEach(book => {
+    book.addEventListener('click', () => {
+        window.open('https://www.dropbox.com/home');
+    });
+});
